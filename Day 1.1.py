5033c609dc9cc6f73c2a80b91752fbcf28f650f3
QKM <qkm@hust.edu.cn>
2024-12-03 12:21:18 +0800

    Merge branch '加速'

--- Day 1.1.py
+++ Day 1.1.py
@@ -1,8 +1,13 @@
 # -*- coding: gbk -*-
 import numpy as np
 
+
 class Variable:
     def __init__(self, data):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError('{} is not supported'.format(type(data)))
+
         self.data = data
         self.grad = None
         self.creator = None
@@ -11,18 +16,30 @@ class Variable:
         self.creator = func
 
     def backward(self):
-        f = self.creator 
-        if f is not None:
-            x = f.input 
-            x.grad = f.backward(self.grad)  
-            x.backward() 
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+
+        funcs = [self.creator]
+        while funcs:
+            f = funcs.pop()
+            x, y = f.input, f.output
+            x.grad = f.backward(y.grad)
+
+            if x.creator is not None:
+                funcs.append(x.creator)
+
+
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
 
 
 class Function:
     def __call__(self, input):
         x = input.data
         y = self.forward(x)
-        output = Variable(y)
+        output = Variable(as_array(y))
         output.set_creator(self)
         self.input = input
         self.output = output
@@ -57,16 +74,15 @@ class Exp(Function):
         return gx
 
 
-A = Square()
-B = Exp()
-C = Square()
+def square(x):
+    return Square()(x)
+
+
+def exp(x):
+    return Exp()(x)
+
 
 x = Variable(np.array(0.5))
-a = A(x)
-b = B(a)
-y = C(b)
-
-# backward
-y.grad = np.array(1.0)
+y = square(exp(square(x)))
 y.backward()
 print(x.grad)
