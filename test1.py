1f7e57daa6ccb9d45be8a749470e02fea4fd6c80
QKM <qkm@hust.edu.cn>
2024-12-04 13:11:09 +0800

    整合

--- test1.py
+++ test1.py
@@ -1,5 +1,19 @@
 import numpy as np
 from dezero import Variable
+from dezero.utils import plot_dot_graph
 
-x = Variable(np.array(1))
-print(x)  # variable(-2.0)
+x0 = Variable(np.array(1.0))
+x1 = Variable(np.array(1.0))
+y = x0 + x1
+
+x0.name = 'x0'
+x1.name = 'x1'
+y.name = 'y'
+
+txt = plot_dot_graph(y, verbose=False)
+print('txt:', txt)
+# =============================================================================
+# Run this code and you will get a file named add.png in the current directory.
+# Open the file with an image viewer to see the graph.
+# =============================================================================
+
